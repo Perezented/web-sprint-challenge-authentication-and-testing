b835e1e3dce3394eaf5bb39fd18a5eb2a0becbf8
Michael Perez <mperez6473@gmail.com>
2020-06-19 10:33:44 -0500

    Added a users model. Have register connected. installed bcrypt and jsonwebtoken.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcryptjs = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Users = require("../users/usersModel");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+    // implement registration
+    const { username, password } = req.body;
+
+    //  hash user password
+    const rounds = process.env.HASH_ROUNDS || 8;
+    const hash = bcryptjs.hashSync(password, rounds);
+    Users.add({ username, password: hash })
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => res.send(err));
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+    // implement login
 });
 
 module.exports = router;
