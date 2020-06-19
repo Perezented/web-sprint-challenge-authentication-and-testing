1dc9f4c1cdca8a1c3d5e0644d368029958bd8b6e
Michael Perez <mperez6473@gmail.com>
2020-06-19 10:56:40 -0500

    Added information to server js to accept tokens and sessions. have the login logic built but is not passing thru just yet.

--- api/server.js
+++ api/server.js
@@ -1,18 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const authenticate = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-router.js");
+const jokesRouter = require("../jokes/jokes-router.js");
 
+const session = require("express-session");
+const KnexSessionsStore = require("connect-session-knex")(session);
+const dbConnection = require("../data/connection");
 const server = express();
-
+const sessionConfig = {
+    name: "monster",
+    secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
+    cookie: {
+        maxAge: 1000 * 600,
+        secure: process.env.COOKIE_SECURE || false, //  true means only use over https //  true in production
+        httpOnly: true, //JS code on the client cannot access the session cookie
+    }, // 10 min in milliseconds
+    resave: false,
+    saveUninitialiezed: false, //  GDPR compliance
+    store: new KnexSessionsStore({
+        knex: dbConnection,
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 6000, //  delete expired sessions - in milliseconds
+    }),
+};
 server.use(helmet());
-server.use(cors());
 server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", authenticate, jokesRouter);
 
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "home slash of the login page" });
+});
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const bcryptjs = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Users = require("../users/usersModel");
+const consts = require("../config/consts");
 
 router.post("/register", (req, res) => {
     // implement registration
@@ -18,7 +19,57 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-    // implement login
+    const { username, password } = req.body;
+
+    //  verify user password
+    Users.findBy({ username })
+        .then(([user]) => {
+            console.log(user);
+            req.session.user = { user };
+            if (user && bcryptjs.compareSync(password, user.password)) {
+                const token = createToken(user);
+                res.status(200).json({
+                    user,
+                    session: req.session,
+                    token: token,
+                });
+            } else
+                res.status(401).json({
+                    Error:
+                        "The username and password combination was not found in our database.",
+                });
+        })
+        .catch((err) => {
+            // console.log(err);
+            res.status(500).json({ error: "could not login", err });
+        });
 });
 
+router.get("/logout", (req, res) => {
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({
+                    Error: "Could not log out, please try again...",
+                });
+            } else res.status(204).end();
+        });
+    } else {
+        req.status(204).end();
+    }
+});
+
+function createToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        description: user.description,
+    };
+    const secret = consts.jwtSecret;
+    const options = {
+        expiresIn: "6h",
+    };
+    return jwt.sign(payload, secret, options);
+}
 module.exports = router;
